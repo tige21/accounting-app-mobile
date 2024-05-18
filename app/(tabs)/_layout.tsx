a2e11eb0b5059f6bab6ff7b3ba5461d7a8886c3a
bondarika <bondarika1@gmail.com>
2024-05-18 20:28:11 +0300

    add plus icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,12 @@
+import SvgComponent from '@/assets/images/svg/add'
 import Colors from '@/constants/Colors'
 import {
-	Ionicons,
 	MaterialCommunityIcons,
 	MaterialIcons,
-	AntDesign
 } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 import React from 'react'
+import { View, StyleSheet } from 'react-native'
 
 export default function TabLayout() {
 	return (
@@ -15,7 +15,7 @@ export default function TabLayout() {
 			screenOptions={{
 				tabBarActiveTintColor: Colors.violet,
 				tabBarShowLabel: false,
-				tabBarStyle: { height: 78 }
+				tabBarStyle: { height: 100 }
 			}}
 		>
 			<Tabs.Screen
@@ -24,11 +24,13 @@ export default function TabLayout() {
 					tabBarLabel: 'Decks',
 					headerShown: false,
 					tabBarIcon: ({ size, color }) => (
-						<MaterialCommunityIcons
-							name='chart-donut'
-							size={40}
-							color={Colors.grey}
-						/>
+						<View style={styles.iconContainer}>
+							<MaterialCommunityIcons
+								name='chart-donut'
+								size={40}
+								color={Colors.grey}
+							/>
+						</View>
 					)
 				}}
 			/>
@@ -38,7 +40,9 @@ export default function TabLayout() {
 					tabBarLabel: 'Decks',
 					headerShown: false,
 					tabBarIcon: ({ size, color }) => (
-						<AntDesign name='pluscircle' size={40} color={Colors.violet} />
+						<View style={styles.plusIconContainer}>
+							<SvgComponent />
+						</View>
 					)
 				}}
 			/>
@@ -48,10 +52,28 @@ export default function TabLayout() {
 					tabBarLabel: 'Decks',
 					headerShown: false,
 					tabBarIcon: ({ size, color }) => (
-						<MaterialIcons name='bar-chart' size={50} color={Colors.grey} />
+						<View style={styles.iconContainer}>
+							<MaterialIcons name='bar-chart' size={50} color={Colors.grey} />
+						</View>
 					)
 				}}
 			/>
 		</Tabs>
 	)
 }
+
+const styles = StyleSheet.create({
+	iconContainer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	plusIconContainer: {
+		position: 'absolute',
+		bottom: 33, 
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: '100%',
+		height: '100%'
+	}
+})
